b46425a7e4aa6ee8d6698a54c870a924d09f9dfc
joshua.rosqueta <joshua.rosqueta@achievewithoutborders.com>
2022-03-02 21:11:58 +0800

    added common folder

---
+++ Santa Archiving System/common/constants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Santa_Archiving_System.common
+{
+    class constants
+    {
+        public static string connectionStringOffline = (@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\SantaLocalDb.mdf;Integrated Security=True");
+        public static string connectionStringOnline = "host='santa-database-do-user-9937190-0.b.db.ondigitalocean.com';" +
+            "Database='SantaDb';" +
+           "port='25060';" +
+            "UID='Santaadmin';" +
+            "PWD='1bU0WiS4jmZkoXha';";
+
+    }
+}
