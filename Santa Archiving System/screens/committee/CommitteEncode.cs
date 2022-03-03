c1c42a6a1cb2e03e4cd4d2dda2b7f5f3555b0ed8
joshua.rosqueta <joshua.rosqueta@achievewithoutborders.com>
2022-03-03 19:40:59 +0800

    added forms for each module

---
+++ Santa Archiving System/screens/committee/CommitteEncode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Santa_Archiving_System.screens.committee
+{
+    public partial class CommitteEncode : Form
+    {
+        public CommitteEncode()
+        {
+            InitializeComponent();
+        }
+    }
+}
