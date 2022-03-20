0616409b9fbd3fdc1488c30a37ac2d32cea0b3aa
joshua.rosqueta <joshua.rosqueta@achievewithoutborders.com>
2022-03-20 17:53:34 +0800

    added sending to email

--- Santa Archiving System/screens/dashboard/dashboard.cs
+++ Santa Archiving System/screens/dashboard/dashboard.cs
@@ -1,4 +1,6 @@
-﻿using Santa_Archiving_System.screens.electronic;
+﻿using Santa_Archiving_System.models;
+using Santa_Archiving_System.screens.electronic;
+using Santa_Archiving_System.screens.sendToEmail;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -23,6 +25,44 @@ namespace Santa_Archiving_System.screens.dashboard
         {
 
         }
-       
+
+        private void guna2Panel1_MouseEnter(object sender, EventArgs e)
+        {
+            lb_resolution.ForeColor = Color.FromArgb(55, 81, 255);
+            lb_resolution_count.ForeColor = Color.FromArgb(55, 81, 255);
+        }
+
+        private void guna2Panel1_MouseLeave(object sender, EventArgs e)
+        {
+            lb_resolution.ForeColor = Color.FromArgb(159, 160, 182);
+            lb_resolution_count.ForeColor = Color.Black;
+        }
+
+        private void guna2Panel1_Click(object sender, EventArgs e)
+        {
+            EmailContent emailContent = new EmailContent()
+            {
+                Type = "Resolution"
+            };
+            SendToEmail sendToEmail = new SendToEmail(emailContent);
+            sendToEmail.ShowDialog();
+        }
+
+        private void guna2Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void guna2Panel2_MouseEnter(object sender, EventArgs e)
+        {
+            lb_ordinance.ForeColor = Color.FromArgb(55, 81, 255);
+            lb_ordinance_count.ForeColor = Color.FromArgb(55, 81, 255);
+        }
+
+        private void guna2Panel2_MouseLeave(object sender, EventArgs e)
+        {
+            lb_ordinance.ForeColor = Color.FromArgb(159, 160, 182);
+            lb_ordinance_count.ForeColor = Color.Black;
+        }
     }
 }
---
+++ Santa Archiving System/screens/sendToEmail/SendToEmail.cs
@@ -0,0 +1,60 @@
+﻿using Santa_Archiving_System.models;
+using Santa_Archiving_System.services.ordinance;
+using Santa_Archiving_System.services.resolution;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Santa_Archiving_System.screens.sendToEmail
+{
+    public partial class SendToEmail : Form
+    {
+        EmailContent emailContent;
+        public SendToEmail(EmailContent data)
+        {
+            this.emailContent = data;
+            InitializeComponent();
+        }
+
+        private async void SendToEmail_Load(object sender, EventArgs e)
+        {
+            lb_data.Text = emailContent.Type;
+            loading1.Visible = true;
+            switch (emailContent.Type) {
+                case "Resolution":
+                    guna2DataGridView1.DataSource = await Resolutions.getListOnline();
+                    break;
+                case "Ordinance":
+                    guna2DataGridView1.DataSource = await Ordinances.getListOrdinanceOnline();
+                    break;
+            }
+            loading1.Visible = false;
+        }
+
+        private void btn_send_Click(object sender, EventArgs e)
+        {
+            EmailContents emailContents = new EmailContents(emailContent);
+            emailContents.ShowDialog();
+        }
+
+        private void guna2DataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            foreach (DataGridViewRow item in this.guna2DataGridView1.SelectedRows)
+            {
+                emailContent = new EmailContent()
+                {
+                    Id = int.Parse(item.Cells[0].Value.ToString()),
+                    Type = "Resolution",
+                    FileName = item.Cells[3].Value.ToString(),
+                    DocType = item.Cells[7].Value.ToString()
+                };
+            }
+        }
+    }
+}
